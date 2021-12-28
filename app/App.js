85497ac1a8743eb8f4567d6cf3800eedb8ee8dcf
Bradley Plunkett <bradleyplunkett@Bradleys-MacBook-Pro.local>
2021-12-27 18:56:47 -0700

    trying to replicate Jr phase final project

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import PlanetComponent from './components/planetComponent';
+import PlanetComponent from './components/SunandPlanets';
 
 function App() {
   return (
