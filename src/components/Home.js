80465dfcc50e08fd64f6f55980348016272cb4cc
Bradley Plunkett <bradleyplunkett@Bradleys-MacBook-Pro.local>
2021-12-30 10:23:48 -0700

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import PlanetComponent from './SunandPlanets.js';
-
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <div className='planetContainer'>
-            <div className='planet'>
-                <PlanetComponent className='planet'/>
+        <React.Fragment>
+            <div className='planetContainer'>
+                <div className='planet'>
+                    <PlanetComponent className='planet' />
+                </div>
             </div>
-        </div>)
+
+            <Link to = '/portfolio'> Click here to go to my portfolio</Link>
+
+        </React.Fragment>
+    )
 }
 
 export default Home
