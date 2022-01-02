bbbc27f59bdfd85e338148910faea52a8c380798
Bradley Plunkett <bradleyplunkett@Bradleys-MacBook-Pro.local>
2022-01-02 16:58:42 -0700

    added ourCity and WonderfulFlowers

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -6,10 +6,14 @@ const MyWork = () => {
         <section className="myWorkClass" id="myWork">
             <div>
                 <p >
-                    My Projects           </p>
-                    <img className = 'wonderfulFlowers' src = {'url(' + require('../keyboardpic.png') + ')'} alt = 'wonderfulFlowers'></img>
-            </div>
-        </section>
+                    My Projects
+                </p>
+                <p>Wonderful Flowers</p>
+                <img className='wonderfulFlowers' src={require('../wonderfulFlowers.png')} alt='wonderfulFlowers' onClick={() => window.open("http://wonderful-flowers.herokuapp.com/", "_blank")} />
+                <p>ourCity</p>
+                <img className='ourCity' src={require('../ourCity.png')} alt='ourCity' onClick={() => window.open("https://www.youtube.com/watch?v=-ov5oKu4JBY", "_blank")} />  
+        </div>
+        </section >
     )
 }
 
