ad6c4b5ec3d529f4d6a3be84dba6d1fb840b16fe
Bradley Plunkett <bradleyplunkett@Bradleys-MacBook-Pro.local>
2022-01-02 12:25:46 -0700

    added i frame to resume

--- src/components/MyResume.js
+++ src/components/MyResume.js
@@ -7,6 +7,11 @@ const MyResume = () => {
             <div>
                 <p >
                     This is where you can view my resume.            </p>
+                    <div className = 'ResumeContainer'>
+                    <iframe title = 'resume' src="https://resume.creddle.io/embed/egxl59xhe7m"
+  width="850" height="1100" seamless></iframe>
+                    </div>
+                
             </div>
         </section>
     )
