5f4abb013f9c4950f2038fb17602b6512ade5f1e
Bradley Plunkett <bradleyplunkett@Bradleys-MacBook-Pro.local>
2022-01-27 09:52:55 -0700

    updated

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -5,14 +5,25 @@ const MyWork = () => {
     return (
         <section className="myWorkClass" id="myWork">
             <div>
-                <p >
+                <p className='MyProjects'>
                     My Projects
                 </p>
                 <p>Wonderful Flowers</p>
-                <img className='wonderfulFlowers' src={require('../wonderfulFlowers.png')} alt='wonderfulFlowers' onClick={() => window.open("http://wonderful-flowers.herokuapp.com/", "_blank")} />
                 <p>ourCity</p>
-                <img className='ourCity' src={require('../ourCity.png')} alt='ourCity' onClick={() => window.open("https://www.youtube.com/watch?v=-ov5oKu4JBY", "_blank")} />  
-        </div>
+                <img className='ourCity' src={require('../ourCity.png')} alt='ourCity' onClick={() => window.open("https://www.youtube.com/watch?v=-ov5oKu4JBY", "_blank")} />
+
+                <div className='card'>
+                    <div className='card-top'>
+                        <h1>Card</h1>
+                    </div>
+                    <div className='card-body'>
+                        <img className='wonderfulFlowers' src={require('../wonderfulFlowers.png')} alt='wonderfulFlowers' onClick={() => window.open("http://wonderful-flowers.herokuapp.com/", "_blank")} />
+                    </div>
+                    <div>
+                        <p> Card component rendered inside a container </p>
+                    </div>
+                </div>
+            </div>
         </section >
     )
 }
