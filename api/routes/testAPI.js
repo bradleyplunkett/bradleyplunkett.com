4674410fbffa463b4429eebbd972dd95ddca42ec
bradleyplunkett <brad.plunkett@datadoghq.com>
2023-03-09 13:05:37 -0700

    changed the site to V1 of Five Points Security Systems

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+    res.send("Affordable Solutions to Protect your Home in the Mile High City!");
 });
 
 module.exports = router;
